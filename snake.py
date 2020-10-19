18bb24ef513e903cd537452a96777c63fcf0a5a9
jdbaptista <baptistajaden@gmail.com>
2020-10-18 21:45:18 -0700

    Final v.1 changes
    Rewrote all the code (removed global variables and created a more functional screen system). Added icon.

--- snake.py
+++ snake.py
@@ -1,9 +1,38 @@
 import tkinter as tk
 from random import randint
 
-class Snake:
-    """ The main player """
+class Screen:
 
+    def __init__(self, canvas):
+        canvas = canvas
+        self.menu_screen()
+    
+    def menu_screen(self):
+        canvas.delete("all")
+        canvas.create_image(240, 70, image=title)
+        canvas.create_image(240, 170, image=play)
+        root.bind("<Key>", menu_keys)
+
+    def game_screen(self):
+        canvas.delete("all")
+        root.bind("<Key>", game_keys)
+        direction = "up"
+        player = Snake()
+        food1 = Food("food1", player)
+        food2 = Food("food2", player)
+        food3 = Food("food3", player)
+        food4 = Food("food4", player)
+        food5 = Food("food5", player)
+        game_loop(player, food1, food2, food3, food4, food5)
+
+    def end_screen(self, score):
+        root.bind("<Key>", end_keys)
+        canvas.delete("all")
+        canvas.create_image(240, 170, image=play)
+        canvas.create_text(240, 240, fill="green", font="Times 20 bold", text="Score: " + str(score))
+
+class Snake:
+    
     def __init__(self):
         """Initiates the snake, initial coords at the middle of the canvas and direction of up"""
         self.x = [240]
@@ -12,28 +41,14 @@ class Snake:
         self.length = 1
         self.direction = "up"
 
-    def update(self):
-        """Binds the snake to Refresh()"""
-        self.movement_logic()
-        self.self_detection()
-        self.draw()
-
     def draw(self):
         """refresh snake and put any changes to attributes to screen"""
         i = self.length
-        game_canvas.delete("Snake")
+        canvas.delete("Snake")
         while i >= 1:
-            game_canvas.create_rectangle(self.x[i - 1], self.y[i - 1], self.x[i - 1] + 21,
+            canvas.create_rectangle(self.x[i - 1], self.y[i - 1], self.x[i - 1] + 21,
                                          self.y[i - 1] + 21, fill="Green", width=0, tags="Snake")
             i -= 1
-
-    def self_detection(self):
-        """Check to see if the head of the snake is overlapping any of the other parts"""
-        i = 1
-        while i < len(self.x):
-            if self.x[0] == self.x[i] and self.y[0] == self.y[i]:
-                end()
-            i += 1
 
     def movement_logic(self):
         """Moves each part of the snake, following the head moving in a specified direction"""
@@ -59,7 +74,7 @@ class Snake:
             self.x.insert(0, temp_x_0 - 20)
             self.y.insert(0, temp_y_0)
 
-    def check_food(self):
+    def check_food(self, food1, food2, food3, food4, food5):
         """Checks to see if the snake has eaten any of the 5 foods"""
         if self.x[0] == food1.x and self.y[0] == food1.y:
             food1.is_eaten()
@@ -78,18 +93,19 @@ class Snake:
             self.adding_length = True
 
 class Food:
+
     counter = 0
 
-    def __init__(self, tag):
+    def __init__(self, tag, player):
         """Defines the food, tag is needed for Tkinter to be able to delete off canvas"""
         Food.counter += 1
         self.x = 0
         self.y = 0
         self.tag = tag
-        self.init_coords()
-        self.draw()
+        self.player = player
+        self.init_coords(self.player)
 
-    def init_coords(self):
+    def init_coords(self, player):
         """Finds suitable coordinates for the food, if none is found the food is deleted forever"""
         space = 576 - player.length - Food.counter
         if space <= 0:
@@ -110,140 +126,97 @@ class Food:
                     break
 
     def draw(self):
-        game_canvas.create_rectangle(self.x, self.y, self.x + 21, self.y + 21,
+        canvas.create_rectangle(self.x, self.y, self.x + 21, self.y + 21,
                                      fill="blue", width=0, tags=self.tag)
 
     def is_eaten(self):
         """Reinitializes the food after it is eaten"""
         # The Snake class deals with checking and adding length
-        game_canvas.delete(self.tag)
+        canvas.delete(self.tag)
         Food.counter -= 1
-        self.__init__(self.tag)
-
-def key(event):
-    """Key bindings"""
-    global direction
-    if event.keycode == 87 and player.direction != "down":  # key pressed is 'W'
-        direction = "up"
-    elif event.keycode == 65 and player.direction != "right":  # key pressed is 'A'
-        direction = "left"
-    elif event.keycode == 83 and player.direction != "up":  # key pressed is 'S'
-        direction = "down"
-    elif event.keycode == 68 and player.direction != "left":  # key pressed is 'D'
-        direction = "right"
-
-def Refresher_init():
-    global player
-    global food1
-    global food2
-    global food3
-    global food4
-    global food5
-    global direction
-
-    direction = "up"
-    # create the player
-    player = Snake()
-
-    # create the food
-    food1 = Food("food1")
-    food2 = Food("food2")
-    food3 = Food("food3")
-    food4 = Food("food4")
-    food5 = Food("food5")
-
-    # establish game loop and key binds
-    root.bind("<Key>", key)
-    Refresher()
-
-def Refresher():
-    global player
-    global food1
-    global food2
-    global food3
-    global food4
-    global food5
-    """The main game loop, refreshing the canvas every 100 milliseconds"""
-    # should change this so that the frame rate and player movement speed are separate
-    if direction == "up" and player.y[0] <= 0:
-        end()
-    elif direction == "down" and player.y[0] >= 460:
-        end()
-    elif direction == "right" and player.x[0] >= 460:
-        end()
-    elif direction == "left" and player.x[0] <= 0:
-        end()
-    else:
-        player.direction = direction
-        player.update()
-        player.check_food()
-    root.after(100, Refresher)
+        self.__init__(self.tag, self.player)
 
 def menu_keys(event):
-    global choice
     if event.keycode == 32:
-        choice = "game"
+        screen.game_screen()
 
+def game_keys(event):
+    global direction
+    if event.keycode == 87:  # key pressed is 'W'
+        direction = "up"
+    elif event.keycode == 65:  # key pressed is 'A'
+        direction = "left"
+    elif event.keycode == 83:  # key pressed is 'S'
+        direction = "down"
+    elif event.keycode == 68:  # key pressed is 'D'
+        direction = "right"
 
-def main_menu():
-    if choice == "game":
-        game_canvas.delete("all")
-        Refresher_init()
+def end_keys(event):
+    if event.keycode == 32:
+        screen.menu_screen()
+
+def game_loop(player, food1, food2, food3, food4, food5, command=""):
+    global direction
+
+    if command == "end":
+        screen.end_screen(player.length - 1)
+        del player
+        del food1
+        del food2
+        del food3
+        del food4
+        del food5
+
+    if direction == "up" and player.direction != "down":
+        player.direction = "up"
+    elif direction == "left" and player.direction != "right":
+        player.direction = "left"
+    elif direction == "down" and player.direction != "up":
+        player.direction = "down"
+    elif direction == "right" and player.direction != "left":
+        player.direction = "right"
+
+    if player.direction == "up" and player.y[0] <= 0:
+        game_loop(player, food1, food2, food3, food4, food5, "end")
+    elif player.direction == "down" and player.y[0] >= 460:
+        game_loop(player, food1, food2, food3, food4, food5, "end")
+    elif player.direction == "right" and player.x[0] >= 460:
+        game_loop(player, food1, food2, food3, food4, food5, "end")
+    elif player.direction == "left" and player.x[0] <= 0:
+        game_loop(player, food1, food2, food3, food4, food5, "end")
     else:
-        root.after(100, main_menu)
+        player.movement_logic()
+        
+        # Check to see if the head of the snake is overlapping any of the other parts
+        i = 1
+        while i < len(player.x):
+            if player.x[0] == player.x[i] and player.y[0] == player.y[i]:
+                game_loop(player, food1, food2, food3, food4, food5, "end")
+            i += 1
 
-def end():
-    global player
-    global food1
-    global food2
-    global food3
-    global food4
-    global food5
-    global choice
-    global title
-    global game_canvas
-    global w_key
-    game_canvas.delete("all")
-
-    title = tk.PhotoImage(file="title.png")
-    game_canvas.create_image(240, 70, image=title)
-
-    w_key = tk.PhotoImage(file="play.png")
-    game_canvas.create_image(240, 170, image=w_key)
-
-    game_canvas.create_text(240, 240, fill="green", font="Times 20 italic bold", text="Score: " + str(player.length - 1))
-    choice = ""
-
-    del player
-    del food1
-    del food2
-    del food3
-    del food4
-    del food5
-
-    root.bind("<Key>", menu_keys)
-    main_menu()
-    
-
-if __name__ == "__main__":
-    # create the window
+        player.draw()
+        food1.draw()
+        food2.draw()
+        food3.draw()
+        food4.draw()
+        food5.draw()
+        player.check_food(food1, food2, food3, food4, food5)
+    root.after(100, lambda: game_loop(player, food1, food2, food3, food4, food5))
+                           
+if __name__ == '__main__':
     root = tk.Tk()
+    root.resizable(False, False)
     root.title("Snake")
-    # icon = tk.PhotoImage(file="snake_icon.gif")
-    # root.iconphoto(False, icon)
-    game_canvas = tk.Canvas(root, width=480, height=480, bg="black")
-    game_canvas.pack()
+    icon = tk.PhotoImage(file="icon.png")
+    root.iconphoto(False, icon)
 
+    canvas = tk.Canvas(root, width=480, height=480, bg="black")
     title = tk.PhotoImage(file="title.png")
-    game_canvas.create_image(240, 70, image=title)
+    play = tk.PhotoImage(file="play.png")
+    canvas.pack()
 
-    w_key = tk.PhotoImage(file="play.png")
-    game_canvas.create_image(240, 170, image=w_key)
+    screen = Screen(canvas)
 
-    choice = ""
     direction = "up"
-
-    root.bind("<Key>", menu_keys)
-    main_menu()
 
     root.mainloop()
